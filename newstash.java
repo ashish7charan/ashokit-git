d528eb75e1e7eb030b42246e7daeba7ceefbe081
Ashish Charan <ashish7charan@gmail.com>
2023-03-05 22:09:56 +0530

    added git stash data + new data

--- newstash.java
+++ newstash.java
@@ -1,3 +1,14 @@
 after adding the file in git stash.
 
 i am performing anaother task here.
+
+hello hi am doing something for gut stash.
+hello there is some i am going to add.
+
+my class {
+i=100
+j=200
+k=300
+
+}
+
